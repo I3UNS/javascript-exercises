eeb6328c8959b1de447fcad5e508bb16846c778b
I3UNS <mohd.assad9876@gmail.com>
2025-12-06 12:41:22 +0600

    Completed exercise 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, strIteratorValue) {
+    let finalStr = "";
 
+    if(strIteratorValue < 0){
+        return "ERROR";
+    } 
+    else {
+        for (let i = 0; i < strIteratorValue; i++){
+        finalStr += str;
+    }
+    }
+
+    return finalStr;
 };
 
 // Do not edit below this line
