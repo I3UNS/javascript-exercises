09d790b848750b525b16ae3a77d536867ba7e0b9
I3UNS <mohd.assad9876@gmail.com>
2025-12-15 15:33:07 +0600

    Completed exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
+const calculateAge = function(deathYear, birthYear){
+    if(!deathYear){
+        deathYear = new Date().getFullYear();
+    }
+    return deathYear - birthYear;
+};
+
 const findTheOldest = function(people) {
-    let arrObj = people.map(el => ({name: el.name ,age: (el.yearOfDeath - el.yearOfBirth)}));
-    const oldestAge = arrObj.sort((a, b) => b.age - a.age);
+    // let arrObj = people
+    //                     .map(el => el.yearOfDeath - el.yearOfBirth)
+    //                     .sort((a, b) => b - a);
+    
+    return people.reduce((oldest, currPerson) => {
+        const oldestAge = calculateAge(oldest.yearOfDeath, oldest.yearOfBirth);
+        const currPersonAge = calculateAge(currPerson.yearOfDeath, currPerson.yearOfBirth);
+        return currPersonAge > oldestAge ? currPerson : oldest;
+    })
     
 };
 
